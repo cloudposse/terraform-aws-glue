a970cd33cbc885c60f463f243d72427720b04624
Yousaf Syed <yousafsyed@icloud.com>
2024-01-08 10:32:09 +0100

    Update variables-deprecated.tf

--- modules/glue-catalog-table/variables-deprecated.tf
+++ modules/glue-catalog-table/variables-deprecated.tf
@@ -3,6 +3,6 @@ variable "partition_index" {
     index_name = string
     keys       = list(string)
   })
-  description = "(Deprecated) Configuration block for a maximum of 3 partition indexes.)"
+  description = "(Deprecated) Configuration block for a maximum of 3 partition indexes."
   default     = null
 }
